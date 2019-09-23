6b5a24b3f48a31abd1d4daf7c82d4a81f938a14e
eduardopcarmo <ecarmo@outlook.com>
2019-09-23 09:54:55 -0700

    Create docker-compose and did all the initial configuration

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# dishlist.wordpress
+# DishList.Wordpress
+
+You need Docker to run this application.
+
+---
+
+#### First Time Usage
+
+1: Run `docker-compose up -d` in the project root directory. Wait for the containers to be built.
+
+---
+
+#### Running Worpress Website
+
+1: You'll be able to access in the address: `http://localhost:8000` (Website) and `http://localhost:8000/wp-admin/` (Dashboard)
+2: Use **User**: team4 and **Password**: \$zOXx#PgHUSMWC3TC5
+
+---
+
+#### Running PhpMyAdmin
+
+1: You'll be able to access in the address: `http://localhost:8080`
+2: Use **User**: root and **Password**: langara
---
+++ wp/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'team4');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'langara');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '46c24358222db1ce6dec2a878210368816216035');
+define( 'SECURE_AUTH_KEY',  '37a70928d3a9511fd559a646b8c9bd21ffc7aa42');
+define( 'LOGGED_IN_KEY',    'd02b83f8fb346655585788db996723ef2897566c');
+define( 'NONCE_KEY',        'f6086b5cce7d93215cfcce2e3068a851da7839b2');
+define( 'AUTH_SALT',        '4948f266a01f1a6a2a5aeb5e3d86f7ad764d8571');
+define( 'SECURE_AUTH_SALT', 'e86883d98df306007785fd8314f1c0bb1ae5026b');
+define( 'LOGGED_IN_SALT',   '327ecd8296736962a9188f8c5173f9d771e7265e');
+define( 'NONCE_SALT',       'd9a7b58093f66f6ba81dc5e49b3de13b6d69bf6a');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
