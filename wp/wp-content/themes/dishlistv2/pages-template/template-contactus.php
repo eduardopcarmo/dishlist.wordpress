bfb0d692446e29cd05459dfd28dc3f4d2446addb
Heloysa Tancredo - LoLo T <heloysatancredo@gmail.com>
2019-11-01 20:00:54 -0700

    Added input[type=email] on _main.scss with other input types to get the same style, and added settings to _contact-page.scss style the contact form

--- wp/wp-content/themes/dishlistv2/pages-template/template-contactus.php
+++ wp/wp-content/themes/dishlistv2/pages-template/template-contactus.php
@@ -3,10 +3,10 @@
 get_header();
 ?>
 
-<main>
+<main class="contact">
 
 <?php
-echo '<h1>Contact Us</h1>';
+echo '<h1 class= "title">Contact Us</h1>';
 
 echo do_shortcode('[contact-form-7 id="249" title="Contact form 1"]');
 get_footer();
