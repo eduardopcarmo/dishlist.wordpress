c7933e1bcf0a568cfefe86173fafa6e580171f6c
Eduardo Pereira do Carmo <40062979+eduardopcarmo@users.noreply.github.com>
2019-11-13 11:22:45 -0800

    our team + media queries in a separeted file

--- wp/wp-content/themes/dishlistv2/pages-template/template-ourteam.php
+++ wp/wp-content/themes/dishlistv2/pages-template/template-ourteam.php
@@ -2,8 +2,43 @@
 
 <?php get_header(); ?>
 
+
+<main class="our-team-users">
+   <h1 class="title"><?= get_the_title(); ?></h1>
+   <div class="our-team-users-container">
+      <?php 
+         // Get all Users (type = subscriber | Order by = name)
+         $blogusers = get_users('role=subscriber&orderby=name');
+         // Array of WP_User objects.
+         foreach ($blogusers as $user) {
+            ?>
+            <div class="user-box">
+               <div class="user-box-photo">
+                  <img src=<?= get_user_meta($user->ID, 'user_photo', true);?>>
+                  <h2><?= $user->first_name . ' ' . $user->last_name;?></h2>
+                  <p>
+                     <?= get_user_meta($user->ID, 'user_position', true); ?>
+                  </p>
+               </div>
+               <div class="user-box-description">
+                  <img src=<?= get_user_meta($user->ID, 'user_dish', true);?>>
+                  <p><?= $user->description; ?></p>
+               </div>
+            </div>
+            <?php
+         }
+      ?>
+   </div>
+</main>
+<br>
+<br>
+<br>
+<br>
+
+
 <main class="our-team">
    <h1 class="title">Our Team</h1>
+
 
    <?php
       $blogusers = get_users('role=subscriber&orderby=name');
@@ -48,6 +83,7 @@
       $count++;
       endforeach;
    ?>
+
 
 </main>
 
--- wp/wp-content/themes/dishlistv2/pages-template/template-update-users.php
+++ wp/wp-content/themes/dishlistv2/pages-template/template-update-users.php
@@ -2,31 +2,31 @@
 <?php 
    // Marcos
    update_user_meta( 2, 'user_position', 'Lead QA & Developer');
-   update_user_meta( 2, 'user_photo', 'http://localhost:8000/wp-content/uploads/2019/11/marcos.jpg');
-   update_user_meta( 2, 'user_dish', 'http://localhost:8000/wp-content/uploads/2019/11/marcos-dish.png');
+   update_user_meta( 2, 'user_photo', '/wp-content/uploads/2019/11/marcos.jpg');
+   update_user_meta( 2, 'user_dish', '/wp-content/uploads/2019/11/dish_marcos.png');
    // Eduardo
    update_user_meta( 3, 'user_position', 'Developer');
-   update_user_meta( 3, 'user_photo', 'http://localhost:8000/wp-content/uploads/2019/11/eduardo.png');
-   update_user_meta( 3, 'user_dish', 'http://localhost:8000/wp-content/uploads/2019/11/eduardo-dish.png');
+   update_user_meta( 3, 'user_photo', '/wp-content/uploads/2019/11/Eduardo_Pereira_do_Carmo.jpg');
+   update_user_meta( 3, 'user_dish', '/wp-content/uploads/2019/11/dish_eduardo.png');
    // Heloysa
    update_user_meta( 4, 'user_position', 'Project Manager');
-   update_user_meta( 4, 'user_photo', 'http://localhost:8000/wp-content/uploads/2019/11/heloysa.png');
-   update_user_meta( 4, 'user_dish', 'http://localhost:8000/wp-content/uploads/2019/11/heloysa-dish.png');
+   update_user_meta( 4, 'user_photo', '/wp-content/uploads/2019/11/heloysa.png');
+   update_user_meta( 4, 'user_dish', '/wp-content/uploads/2019/11/dish_helo.png');
    // Walter
    update_user_meta( 5, 'user_position', 'Lead Developer');
-   update_user_meta( 5, 'user_photo', 'http://localhost:8000/wp-content/uploads/2019/11/walter.png');
-   update_user_meta( 5, 'user_dish', 'http://localhost:8000/wp-content/uploads/2019/11/walter-dish.png');
+   update_user_meta( 5, 'user_photo', '/wp-content/uploads/2019/11/walter.png');
+   update_user_meta( 5, 'user_dish', '/wp-content/uploads/2019/11/dish_walter.png');
    // Jyot
    update_user_meta( 6, 'user_position', 'Developer');
-   update_user_meta( 6, 'user_photo', 'http://localhost:8000/wp-content/uploads/2019/11/jyot.png');
-   update_user_meta( 6, 'user_dish', 'http://localhost:8000/wp-content/uploads/2019/11/jyot-dish.png');
+   update_user_meta( 6, 'user_photo', '/wp-content/uploads/2019/11/jyot.png');
+   update_user_meta( 6, 'user_dish', '/wp-content/uploads/2019/11/dish_jyot.png');
    // Nazi
    update_user_meta( 7, 'user_position', 'Designer & Developer');
-   update_user_meta( 7, 'user_photo', 'http://localhost:8000/wp-content/uploads/2019/11/nazi.png');
-   update_user_meta( 7, 'user_dish', 'http://localhost:8000/wp-content/uploads/2019/11/nazi-dish.png');
+   update_user_meta( 7, 'user_photo', '/wp-content/uploads/2019/11/nazi.png');
+   update_user_meta( 7, 'user_dish', '/wp-content/uploads/2019/11/dish_nazi.png');
    // Harry
    update_user_meta( 8, 'user_position', 'Lead Designer');
-   update_user_meta( 8, 'user_photo', 'http://localhost:8000/wp-content/uploads/2019/11/harry.png');
-   update_user_meta( 8, 'user_dish', 'http://localhost:8000/wp-content/uploads/2019/11/harry-dish.png');
+   update_user_meta( 8, 'user_photo', '/wp-content/uploads/2019/11/harry.png');
+   update_user_meta( 8, 'user_dish', '/wp-content/uploads/2019/11/dish_harry.png');
    echo "<p>Updated!</p>";
 ?>
