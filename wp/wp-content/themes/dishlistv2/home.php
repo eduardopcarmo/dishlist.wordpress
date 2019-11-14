6661fc1b4ea413605295c21939cb49e4b8018c55
Eduardo Pereira do Carmo <40062979+eduardopcarmo@users.noreply.github.com>
2019-11-14 10:07:58 -0800

    set feature to use .box class

--- wp/wp-content/themes/dishlistv2/home.php
+++ wp/wp-content/themes/dishlistv2/home.php
@@ -22,59 +22,40 @@ get_header();?>
         <section id="feature">
             <span class="home-sb-plate"></span>
             <h1 class="title">Features</h1>
-            <?php
-                $args = array(
-                    'post_type' => 'post',
-                    'post_status' => 'publish',
-                    'category_name' => 'feature',
-                    'posts_per_page' => 10,);
-                    $arr_posts = new WP_Query( $args );
-                    // Get all our posts in "Feature Category", and start the counter
-                    $postNumber = 0;
-                    if ( $arr_posts->have_posts() ) :
-                        echo '<div class="post-area-row">';
-                            echo '<div class="post-col-1"></div>';
-                            while ( $arr_posts->have_posts() ) :
+            <div class="feature-container">
+                <div class="feature-container-list">
+                    <?php
+                        // Get all our posts in "Feature Category"
+                        $args = array(
+                            'post_type' => 'post',
+                            'post_status' => 'publish',
+                            'category_name' => 'feature',
+                            'posts_per_page' => 10
+                        );
+                        $arr_posts = new WP_Query( $args );
+                        if ( $arr_posts->have_posts() ) {
+                            while ( $arr_posts->have_posts() ) {
                                 $arr_posts->the_post();
-                                // If we're at the first post, or just before a post number that is divisible by 2 then we start a new row
-                                $postClass = '';
-                                if ($postNumber == 0 || (($postNumber) % 2 == 0))  {
-                                    $postClass .= "post-col-2";
-                                } else {
-                                    $postClass .= "post-col-3";
-                                }
-                                // Choose the post class based on what number post we are 
-                                echo '<div class="'.$postClass.'">';?>
-                                <article id="post-<?php the_ID(); ?>" class="postshow" <?php post_class(); ?>>
-                                    <div class="post-row">
-                                        <div class="post-thumb">
-                                        <?php
-                                            if ( has_post_thumbnail() ) {
-                                                ?>
-                                                <img  src="<?= get_the_post_thumbnail_url(); ?>" alt="<?= the_title(); ?>">
-                                                <?php
-                                            }
-                                        ?>
-                                        </div>
-                                        <header class="entry-header">
-                                            <h3 class="entry-title"><?php the_title(); ?></h3>
-                                        </header>
-                                        <div class="entry-content">
-                                            <?php the_excerpt(); ?>
-                                        </div>
-                                    </div><!-- post-row -->
-                                </article>
-                                <?php
-                                    echo "</div>";
-                                    $postNumber++; // Increment counter
                                 ?>
-                        <?php
-                            endwhile;
-                        echo '</div>'; // close poat-area-row tag
-                    endif;?>
-
-                    
-        </section><!-- #feature section -->     
+                                <div class="box">
+                                    <?php
+                                        if ( has_post_thumbnail() ) {
+                                            ?>
+                                            <img  src="<?= get_the_post_thumbnail_url(); ?>" alt="<?= the_title(); ?>">
+                                            <?php
+                                        }
+                                    ?>
+                                    <h3><?= the_title(); ?></h3>
+                                    <p><?= get_the_excerpt(); ?></p>
+                                </div>
+                                <?php
+                            }
+                            wp_reset_postdata();
+                        }
+                    ?>
+                </div> 
+            </div>
+        </section>
         <!-- The FQA Link -->
         <section id="faq-area">
             <div class="faq-box">
