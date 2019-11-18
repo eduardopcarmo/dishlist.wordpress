3605e661ef56caf063bf6264a6bc17fca6dc0d7b
Eduardo Pereira do Carmo <40062979+eduardopcarmo@users.noreply.github.com>
2019-11-18 14:24:25 -0800

    correct navigation.js to use the new screen size

--- wp/wp-content/themes/dishlistv2/assets/js/navigation.js
+++ wp/wp-content/themes/dishlistv2/assets/js/navigation.js
@@ -5,35 +5,35 @@
  * Contains handlers for navigation area.
  */
 
-(function( $ ) {
+(function ($) {
 	var clickStat;
-	$(".menu-toggle").click(()=>{
+	$(".menu-toggle").click(() => {
 		var navitems = $(".menu-item");
-		if (navitems.css( "display" ) === "block") {
-			navitems.css( "display", "none" );
-			clickStat=0;
+		if (navitems.css("display") === "block") {
+			navitems.css("display", "none");
+			clickStat = 0;
 		} else {
-			navitems.css( "display", "block" );
-			clickStat=1;
+			navitems.css("display", "block");
+			clickStat = 1;
 		}
 	})
-	
-	$( window ).resize(() =>{
-		var x=document.documentElement.clientWidth;
-		
-		if(x>=768){
-			$(".menu-item").css( "display", "inline-block" );
+
+	$(window).resize(() => {
+		var x = document.documentElement.clientWidth;
+
+		if (x >= 992) {
+			$(".menu-item").css("display", "inline-block");
 		} else {
 			//console.log(x);
-			$(".menu-item").css( "display", "none" );
-			if(clickStat===0){
-				$(".menu-item").css( "display", "none" );
-			} else if (clickStat===1){
-				$(".menu-item").css( "display", "block" );
-			} 
+			$(".menu-item").css("display", "none");
+			if (clickStat === 0) {
+				$(".menu-item").css("display", "none");
+			} else if (clickStat === 1) {
+				$(".menu-item").css("display", "block");
+			}
 		}
-			
+
 
 	})
 
-})( jQuery );
+})(jQuery);
