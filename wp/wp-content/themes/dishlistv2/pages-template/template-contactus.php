3742489ed7a77059cd3982e289f582002aaa8f59
Heloysa Tancredo - LoLo T <heloysatancredo@gmail.com>
2019-11-18 10:33:26 -0800

    Contact page mobile and desktop version

--- wp/wp-content/themes/dishlistv2/pages-template/template-contactus.php
+++ wp/wp-content/themes/dishlistv2/pages-template/template-contactus.php
@@ -1,15 +1,26 @@
 <?php /* Template Name: ContactUs */?>
-<?php
-get_header();
-?>
+<?php get_header();?>
 
 <main class="contact">
+<?php echo '<h1 class= "title contactTitle">Contact Us</h1>';?>
+    <div class="contactForm">
+        <?php echo do_shortcode('[contact-form-7 id="249" title="Contact form 1"]');?>
+    </div>
 
-<?php
-echo '<h1 class= "title">Contact Us</h1>';
+    <div class="contactDetails">
 
-echo do_shortcode('[contact-form-7 id="249" title="Contact form 1"]');
-get_footer();
-?>
+        <h3> Our Address</h3>
+        <div class="address">
+            <p>Langara College</p>
+            <p>100 West 49th Avenue</p>
+            <p>Vancouver B.C.</p>
+            <p>Canada V5Y 2Z6</p>
+            <p>604 323 5511</p>
+            <hr>
+            <p>Phone: +1 778 325 825</p>
+            <p>Email: mail@dishlist.wmdd.com</p>
+        </div>
+
+    </div>
 
 </main>
